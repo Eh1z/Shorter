b603fda9e14c83a7a9a47e5ef7afbc82470bbd94
Eh1z <godsentehiz11@gmail.com>
2023-05-23 20:46:27 +0100

    almost done with the UI

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { copy, linkIcon, loader, tick } from '../assets';
 
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+      {/*Search Bar*/}
+      <div className='w-full flex flex-col gap-3'>
+        <form onSubmit={() => {}}
+        className='flex items-center justify-center relative'
+        >
+          <img src={linkIcon} alt="Link_Icon"
+          className='left-0 my-2 ml-3 w-5 absolute'/>
+          <input type="url" placeholder='Enter URL of Article' 
+          value=""
+          className='url_input peer'
+          onChange={() => {}}
+          required
+          />
+          <button type='submit' className='submit_btn peer-focus: border-purple-700 peer-focus:text-purple-700'>
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAKlJREFUSEtjZKAxYKSx+QyjFhAMYYqDqE7p3H+QLU33jLCaNWoBw9AIojrFcwua7hslYEsyFPmgXuG8wH+m//sZGBgMcKUSsi2oVzhvADVcAF8yJMuCWuXzCYz//88nmIuQFJCUD0i2gJHhYNNdIwdsDsKZ0aBBdICBgYEfXxAR8iXenAyNZJAl+riCgCILYJrxJVOqWEDIEHzyFBd2hCwftYBQCNG+0gcAH1xNGUS+9NYAAAAASUVORK5CYII="/>
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,14 @@ const Hero = () => {
           Github
         </button>
       </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with<br className='max-md:hidden'/>
+        <span className='purple_gradient pt-10'>The Power of GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+      Simplify your reading experience with Shorter, the AI-powered summarizer. Extract key points from any text, effortlessly.
+      </h2>
     </header>
   )
 }
